1776f641a3cf793053d35f829d6897ea22a75d79
ErAdilrasheed <er.adilrasheed@gmail.com>
2024-12-20 00:49:06 +0530

    Initial commit

---
+++ server/multerconfig/storageConfig.js
@@ -0,0 +1,38 @@
+const multer = require("multer");
+
+
+// storage config
+const storage = multer.diskStorage({
+    destination:(req,file,callback)=>{
+        callback(null,"./uploads")
+    },
+    filename:(req,file,callback)=>{
+        const filename = `image-${Date.now()}.${file.originalname}`
+        callback(null,filename)
+    }
+});
+
+// filter 
+const filefilter = (req,file,callback)=>{
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg' ||
+      file.mimetype === 'image/webp' ||
+      file.mimetype === 'image/svg'
+    ) {
+      callback(null, true)
+    } else {
+      callback(null, false)
+      return callback(
+        new Error('Only .png .jpg .webp .svg & .jpeg formatted Allowed')
+      )
+    }
+}
+
+const upload = multer({
+    storage:storage,
+    fileFilter:filefilter
+});
+
+module.exports = upload;
